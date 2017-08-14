15c6860c86313d024bfaf6fbdbf64d88a048dc21
XingZengZhi <944047118@qq.com>
2017-08-14 17:01:13 +0800

    show comments js v1.1

--- ShowComments/ShowComment.js
+++ ShowComments/ShowComment.js
@@ -9,9 +9,11 @@
     this.each(function(){
       //实现各种功能
       var _this = $(this);  //获取调用插件的对象
-      
+      _this.click(function(){
+        console.log("被点击了")
+      });
     });
 
     return this;
   }
-})(jquery);
+})(jQuery);

cc2b4656d12bc6d4e0b58cc70a3b14594ac95846
XingZengZhi <944047118@qq.com>
2017-08-14 23:47:43 +0800

    动态评论插件

--- ShowComments/ShowComment.js
+++ ShowComments/ShowComment.js
@@ -2,18 +2,29 @@
   $.fn.ShowComment = function(options){
     var defaults = {
       //参数和属性
-      currentClass:"comments",
-      itemClass:"commentsItem"
+      itemClass:".commentsItem"
     }
-
     var options = $.extend(defaults, options);
 
     this.each(function(){
       //实现各种功能
       var _this = $(this);  //获取调用插件的对象
-      _this.click(function(){
-        console.log("被点击了")
+      var nums = new Array(_this.find(options.itemClass).length);
+      $.each(_this.find(options.itemClass), function(i, n){
+        nums[i] = i;
       });
+      var time = setTimeout(topMove, 1000), j = 0;
+      function topMove(){
+        _this.find(options.itemClass + ":eq(" + j + ")").animate({
+          marginTop:0 + 'px'
+        });
+        j++;
+        if(j > nums.length){
+          clearTimeout(time);
+        }else{
+          time = setTimeout(topMove, 1000);
+        }
+      }
     });
 
     return this;

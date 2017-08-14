1518a2c9ca205377cc138a028cd0145f886c545d
XingZengZhi <944047118@qq.com>
2017-08-14 16:42:58 +0800

    show comments js

---
+++ ShowComments/ShowComment.js
@@ -0,0 +1,17 @@
+;(function($){
+  $.fn.ShowComment = function(options){
+    var defaults = {
+      //参数和属性
+    }
+
+    var options = $.extend(defaults, options);
+
+    this.each(function(){
+      //实现各种功能
+      var _this = $(this);  //获取调用插件的对象
+      
+    });
+
+    return this;
+  }
+})(jquery);
